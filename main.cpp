b6d44dac2dd4c9c51f1f10ea110a43b77bddb026
vynillin <kostaduba92@gmail.com>
2025-11-13 16:50:57 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
 #include "variable.h"
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int t;
+
+    arq='result.dat';
+    std::ofstream out;
+    out.open("arq", ios::out | ios::app);
+    //out << 4x,"Beta",4x,"Energy",4x,"Specific Heat",4x,"Uniform Susceptibility",  4x,"Magnetization^2",4x,"Number of H-operators", 4x,"(Number of H-operators)^2";
+
+    for (Nn=0; Nn<100; Nn++){
+        for(t=0; t<t_steps;t++){
+            beta = 1.0 / (temp_ini + dt * t);
+            NH = 0;
+            NH_max = 0;
+            L = std::max(4, N / 4);
+
+            Allocate(opstring[0:L - 1])
+            Allocate(first_vertex_visitted[N], last_vertex_visitted[N])
+            Allocate(vertex_link[0:4 * L - 1])
+
+            opstring = 0;
+            lattice();
+            init();
+
+            add_prob = 0.5 * beta * Nb;
+            rem_prob = 1.0 / (0.5 * beta * Nb);
+
+            termalization();
+            qmc_steps();
+            results();
+            make_dist();
+            free_memory();
+        }
+    }
+    out.close();
+
     return 0;
+}
+
+void lattice()
+{
+
 }
--- variable.h
+++ variable.h
@@ -1,8 +1,12 @@
 #ifndef VARIABLE_H_INCLUDED
 #define VARIABLE_H_INCLUDED
-class variables
-{
-public:
+
+    //Critical temperature for a 3D Heisenberg model is Tc = 0.945d0.
+
+    //lx=10, ly=1, lz=1 means a 1D chain lattice with size 10.
+    // lx=10, ly=10, lz=1 means a 2D square lattice.
+    // lx=10, ly=10, lz=10 means a 3D cubic lattice.
+
     constexpr int lx = 16;                     //Number of spins in x.
     constexpr int ly = 16;                     //Number of spins in y.
     constexpr int lz = 16;                     //Number of spins in z.
@@ -12,30 +16,28 @@ public:
     constexpr int N = lx * ly * lz;            // Total number of spins.
     constexpr double temp_ini = 1.05;          // Initial temperature.
     constexpr int t_steps = 1;                 // Number of temperature steps.
-    constexpr double dt = -0.025d0;            // Size of temperature steps.
+    constexpr double dt = -0.025;              // Size of temperature steps.
 
-    int NH;                   // Number of H-operator.
-    int L;                    // Maximum string size. (Don't change it)
-    int d;                    // Dimension. (Selected by the program)
-    int Nb;                   // Number o bonds between spins. (N = d*lx*ly*lz)
+    int NH;                                    // Number of H-operator.
+    int L;                                     // Maximum string size. (Don't change it)
+    int d;                                     // Dimension. (Selected by the program)
+    int Nb;                                    // Number o bonds between spins. (N = d*lx*ly*lz)
     int Nn;
 
-    int spin[];                    // Spin value.
-    int opstring[];                // Operator string ID.
-    int bound_spin_ID[][];         // List of the spin sites connect between the bound_spin_ID(1, bound_idx) and bound_spin_ID(2, bound_idx)
-    int vertex_link[];             // List of vertex links.
-    int first_vertex_visitted[];   // First operator on each site in linked vertex list.
-    int last_vertex_visitted[];    // Last operator on each site in linked vertex list.
+    int *spin;                                 // Spin value.
+    int *opstring;                             // Operator string ID.
+    int **bound_spin_ID;                       // List of the spin sites connect between the bound_spin_ID(1, bound_idx) and bound_spin_ID(2, bound_idx)
+    int *vertex_link;                          // List of vertex links.
+    int *first_vertex_visitted;                // First operator on each site in linked vertex list.
+    int *last_vertex_visitted;                 // Last operator on each site in linked vertex list.
 
     int NH_max;
-    string arq;
+    char arq;
 
     double beta;
     double rem_prob;
     double add_prob;
     double n_opH = 0.0, n_opH2 = 0.0;
     double ususc = 0.0, staggered = 0.0;
-};
-
 
 #endif // VARIABLE_H_INCLUDED
